62b6345c4bd5d25da1cbe5d1c12337f34744c1ea
Janis <Fallyx@users.noreply.github.com>
2018-04-29 15:40:38 +0200

    Added character movement
    TODO: Fix Tileset
    TODO: Fix layout ordering
    tile_level1:  ground, walls
    tile_level2:  wall-/groundedge
    tile_level3: objects on the ground
    tile_level4: monsters and character

---
+++ Goat Mountain/Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    private Vector2 direction;
+    private Rigidbody2D rbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        direction = Vector2.zero;
+        rbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetInput();
+        // Move();
+	}
+
+    private void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+        rbody.velocity = new Vector2(0, 0);
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+            rbody.velocity = new Vector2(0, speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+            rbody.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+            rbody.velocity = new Vector2(0, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+            rbody.velocity = new Vector2(speed, 0);
+        }
+    }
+}
