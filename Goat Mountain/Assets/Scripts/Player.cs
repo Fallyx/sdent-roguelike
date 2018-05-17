a32e6656b80fcf4f7a1c167df9ca84badb297012
Fabio Strappazzon <fabio.strappazzon@students.fhnw.ch>
2018-05-17 16:33:24 +0200

    add shield

--- Goat Mountain/Assets/Scripts/Player.cs
+++ Goat Mountain/Assets/Scripts/Player.cs
@@ -7,28 +7,59 @@ public class Player : MonoBehaviour {
     [SerializeField]
     private float speed;
 
+    [SerializeField]
+    private float blockSpeedFactor;
 
     [SerializeField]
     private float dashSpeed;
+
+    [SerializeField]
+    private int health;
+
+    [SerializeField]
+    private GameObject shield;
 
     private Vector2 direction;
     private Rigidbody2D rbody;
     private bool facingLeft;
     private int dashCooldown = 0;
+    private bool isBlocking = false;
+    private float blockSpeed;
+    private float blockDashSpeed;
+    private float currentSpeed;
+    private float currentDashSpeed;
+
 
 	// Use this for initialization
 	void Start ()
     {
         direction = Vector2.zero;
         rbody = GetComponent<Rigidbody2D>();
+
+        currentSpeed = speed;
+        currentDashSpeed = dashSpeed;
+        blockSpeed = speed * blockSpeedFactor;
+        blockDashSpeed = dashSpeed * blockSpeedFactor;
 	}
+
+
 	
 	// Update is called once per frame
 	void Update ()
     {
+
         GetInput();
         // Move();
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+           
+        }
     }
 
     private void Move()
@@ -60,7 +91,7 @@ public class Player : MonoBehaviour {
             direction += Vector2.right;
         }
         //rbody.velocity = direction.normalized * speed;
-        rbody.AddForce(direction.normalized * speed);
+        rbody.AddForce(direction.normalized * currentSpeed);
         GetComponent<Animator>().SetBool("walking", direction.magnitude > 0.1);
 
         if(direction.x < 0)
@@ -74,11 +105,11 @@ public class Player : MonoBehaviour {
         }
         GetComponent<SpriteRenderer>().flipX = facingLeft;
 
-        if (Input.GetKey(KeyCode.J))
+        if (Input.GetKey(KeyCode.J)) //dash
         {
             if(dashCooldown < 1)
             {
-                rbody.AddForce(direction.normalized * dashSpeed);
+                rbody.AddForce(direction.normalized * currentDashSpeed);
                 dashCooldown = 30;
                 GetComponent<ParticleSystem>().Play();
             }
@@ -89,5 +120,27 @@ public class Player : MonoBehaviour {
             GetComponent<ParticleSystem>().Stop();
         }
         dashCooldown--;
+
+        if (Input.GetKey(KeyCode.K)) //attack
+        {
+
+        }
+
+        bool blockKeyHeld = Input.GetKey(KeyCode.L);
+        if (blockKeyHeld != isBlocking)
+        {
+            isBlocking = blockKeyHeld;
+            shield.SetActive(isBlocking);
+            if (isBlocking)
+            {
+                currentDashSpeed = blockDashSpeed;
+                currentSpeed = blockSpeed;
+            }
+            else
+            {
+                currentSpeed = speed;
+                currentDashSpeed = dashSpeed;
+            }
+        }
     }
 }
