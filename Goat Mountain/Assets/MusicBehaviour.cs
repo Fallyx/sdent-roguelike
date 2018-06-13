1b708628d4f962d3d8ffcd4d4b6b1b036b23a875
Janis <Fallyx@users.noreply.github.com>
2018-06-13 13:39:12 +0200

    Added music
    Added Bat

---
+++ Goat Mountain/Assets/MusicBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBehaviour : MonoBehaviour {
+
+    private float keyPressedTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        keyPressedTime -= Time.deltaTime;
+        if (keyPressedTime <0)
+        {
+            GetInput();
+        }
+	}
+
+    private void GetInput()
+    {
+        if(Input.GetKey(KeyCode.M))
+        {
+            this.gameObject.GetComponent<AudioSource>().mute = !this.gameObject.GetComponent<AudioSource>().mute;
+            keyPressedTime = 0.5f;
+        }
+    }
+}
